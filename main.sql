2dce6491e79c83ba6c0b39da2b7471bc7d538f54
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-01 17:52:13 +0000

    test as subquery

--- main.sql
+++ main.sql
@@ -2,4 +2,4 @@
 A median is defined as a number separating the higher half of a data set from the lower half. Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to  decimal places.
 */
 set @pointer:= -1; 
-select @pointer:=@pointer+1 as pointer, lat_n, round(@pointer/2) from station order by lat_n
+select * from (select @pointer:=@pointer+1 as pointer, lat_n, round(@pointer/2) from station order by lat_n) as t1 where t1.pointer in (round(@pointer/2))
